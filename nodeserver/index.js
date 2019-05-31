dde78adca41533cf5c5e4e560377fb2000000118
Oskar Olsson <oskarolsson86@gmail.com>
2019-05-31 14:44:52 +0200

    Added comments to index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -5,8 +5,11 @@ const db = require('./models');
 
 const app = express();
 
+//Setting the PORT variable, 2000 if unset (Just because React uses 3000 as default).
 const port = process.env.PORT || 2000;
 
+// Using cors to be able to do requests to localhost.
+// Parsing the body to req.body.
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use('/', routes);
 
+// Starting up the database then the server to start listening to requests.
 db.connectDb().then(() => {
   const listener = app.listen(port, () => {
     console.info(`Server is listening on port ${listener.address().port}.`);
